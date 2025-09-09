e3c998e5c8abce1de815f4d0d298393efc9d0b99
huzaifayaseen3 <154008098+huzaifayaseen3@users.noreply.github.com>
2025-09-09 15:43:10 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Main {
     /*
+    (   Recursion Basics (A function repeating it-self  )
 public static  void printIncreasing(int n){
     if(n == 1){
         System.out.println(n);
@@ -11,23 +12,32 @@ public static  void printIncreasing(int n){
 
 }
 
-     */
+
     public static  void printIncreasing(int n){
         System.out.println(n);
-        if(n == 1){
-            System.out.println(n);
-            return   ;
-        }
-        printIncreasing(n-1);
-        //  System.out.println(n);
+if(n == 1){
+    System.out.println(n);
+    return   ;
+}
+printIncreasing(n-1);
+  //  System.out.println(n);
     }
+    printIncreasing(n);
+    System.out.print("Enter  N : " );
+    int n = sc.nextInt();
 
+    Finding      Factorial
+     */
+    static int  factorial(int n){
+        if(n==1 || n==0 ){
+            return 1;
+        }
+        return n * factorial((n-1));
+
+    }
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter  N : " );
         int n = sc.nextInt();
-
-        printIncreasing(n);
+        System.out.println( factorial(n));
     }
 }
