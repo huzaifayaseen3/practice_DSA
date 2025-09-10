c446bf1d52f41e2de4bed1c88297cc8c9fa01603
huzaifayaseen3 <154008098+huzaifayaseen3@users.noreply.github.com>
2025-09-10 23:47:46 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -53,6 +53,7 @@ printIncreasing(n-1);
         System.out.println( Fabonacci(n));
 
      */
+    /*
     static int sumOfDigits(int n){
         if (n > 0 && n <= 9) {
             return n;
@@ -60,10 +61,22 @@ printIncreasing(n-1);
             return sumOfDigits(n / 10) + n % 10;
     }
 
+        System.out.println(sumOfDigits(n));
+     */
 
+        static int pow(int p , int q ){
+            if(q == 0){
+                return 1 ;
+            }
+            return pow(p,q-1) * p ;
+        }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(sumOfDigits(n));
+        //int n = sc.nextInt();
+        System.out.println("Enter pth Term : ");
+        int p = sc.nextInt();
+        System.out.println("Enter qth Term : ");
+        int q = sc.nextInt();
+        System.out.println(pow(p,q));
     }
 }
