b0fe0eb4aadbed98766c3827219f543ca33cbfe2
huzaifayaseen3 <154008098+huzaifayaseen3@users.noreply.github.com>
2025-09-12 11:56:19 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -105,7 +105,7 @@ printIncreasing(n-1);
         }
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    //    Scanner sc = new Scanner(System.in);
         //int n = sc.nextInt();
 //        System.out.println("Enter pth Term : ");
 //        int p = sc.nextInt();
